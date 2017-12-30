0327b7490a313931f1b38741593a6dd8f1bf4179
labrick <yananback@gmail.com>
2017-12-30 11:33:59 +0800

    add some comment

--- Douban2Piratebay.user.js
+++ Douban2Piratebay.user.js
@@ -14,21 +14,25 @@
     'use strict';
 
     function insertAfter(newNode, referenceNode) {
+        // 父节点中插入该节点，切在给定节点之后的节点（br）之前
         referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
     }
     
     function addLink(fragment, text, href){
+        // 复制一个节点
         let a = imdblink.cloneNode(true);
         a.textContent = text;
         a.href = href;
         fragment.appendChild(a);
     }
 
+    // 选择具有某种选项的组件(XPAHT)
     var links =  document.querySelectorAll (
         "#info > a"
     );
     var imdblink = null;
     var imdbRe = new RegExp("tt[0-9]{4,}");
+    // 从搜索出的组件中找到包含的IMDB号码
     for (var i=0; i<links.length; i++) {
         if(imdbRe.test(links[i].textContent)) {
             imdblink = links[i];
@@ -37,17 +41,18 @@
     }
 
     if (imdblink !== null) {
-       var imdbindex = imdblink.innerText;
-       var fragment = document.createDocumentFragment();
-       var br = document.createElement("br");
-       var span = imdblink.previousElementSibling.cloneNode(false);
-       span.textContent = "资源：";
-       fragment.appendChild(br);
-       fragment.appendChild(span);
-       addLink(fragment, "TPB ", 'https://thepiratebay.org/search/' + imdbindex);
-       addLink(fragment, "RARGB ", 'https://rarbgmirror.com/torrents.php?imdb=' + imdbindex);
-       addLink(fragment, "HD湾 ", 'http://www.hdwan.net/?s=' + imdbindex);
-       addLink(fragment, "电影天堂 ", 'http://www.btrr.net/?s=' + imdbindex);
-       insertAfter(fragment, imdblink);
+        var imdbindex = imdblink.innerText;
+        var fragment = document.createDocumentFragment();
+        var br = document.createElement("br");
+        // 选择前一个元素
+        var span = imdblink.previousElementSibling.cloneNode(false);
+        span.textContent = "资源：";
+        fragment.appendChild(br);
+        fragment.appendChild(span);
+        addLink(fragment, "TPB ", 'https://thepiratebay.org/search/' + imdbindex);
+        addLink(fragment, "RARGB ", 'https://rarbgmirror.com/torrents.php?imdb=' + imdbindex);
+        addLink(fragment, "HD湾 ", 'http://www.hdwan.net/?s=' + imdbindex);
+        addLink(fragment, "电影天堂 ", 'http://www.btrr.net/?s=' + imdbindex);
+        insertAfter(fragment, imdblink);
     }
 })();
